6077a11e040aed7961409a50f9d1afba9627bd9f
TylerTomasello <ttomasel@hawk.iit.edu>
2017-11-13 12:35:26 -0600

    Add html element and change onclick to id. And add javascript to do the work

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,12 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+$(document).ready(function(){
+  $("dbell-controller").remove();
+
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
+  $("#ringDoorbell").on("click", function(){
+    ringDoorbell();
+  });
+});
